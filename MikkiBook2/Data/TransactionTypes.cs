747868687175867355cab6b92667954394601412
cmbsolver <cmbsolver@gmail.com>
2024-05-04 22:30:29 -0500

    Added account db.

---
+++ MikkiBook2/Data/TransactionTypes.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MikkiBook2.Data;
+
+    /// <summary>
+    ///  TransactionTypes
+    /// </summary>
+    public class TransactionTypes
+    {
+        /// <summary>Initializes a new instance of the <see cref="TransactionTypes" /> class.</summary>
+        /// <param name="transactionTypeCode">The transaction type code.</param>
+        /// <param name="transactionType">Type of the transaction.</param>
+        /// <param name="isDebit">if set to <c>true</c> [is debit].</param>
+        /// <param name="isCredit">if set to <c>true</c> [is credit].</param>
+        public TransactionTypes(string transactionTypeCode, string transactionType, bool isDebit, bool isCredit)
+        {
+            TransactionTypeCode = transactionTypeCode;
+            TransactionType = transactionType;
+            IsDebit = isDebit;
+            IsCredit = isCredit;
+        }
+
+        /// <summary>Gets or sets the transaction type code.</summary>
+        /// <value>The transaction type code.</value>
+        public string TransactionTypeCode { get; set; }
+
+        /// <summary>Gets or sets the type of the transaction.</summary>
+        /// <value>The type of the transaction.</value>
+        public string TransactionType { get; set; }
+
+        /// <summary>Gets or sets a value indicating whether this instance is debit.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is debit; otherwise, <c>false</c>.</value>
+        public bool IsDebit { get; set; }
+
+        /// <summary>Gets or sets a value indicating whether this instance is credit.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is credit; otherwise, <c>false</c>.</value>
+        public bool IsCredit { get; set; }
+
+        /// <summary>Converts to string.</summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return $"{TransactionTypeCode} - {TransactionType}";
+        }
+
+        /// <summary>Gets the transaction types.</summary>
+        /// <returns>
+        ///   <br />
+        /// </returns>
+        public static TransactionTypes[] GetTransactionTypes()
+        { 
+            List<TransactionTypes> transactionTypes = new List<TransactionTypes>();
+
+            transactionTypes.Add(new TransactionTypes("ATM", "ATM Withdrawl", true, false));
+            transactionTypes.Add(new TransactionTypes("DC", "Debit Card", true, false));
+            transactionTypes.Add(new TransactionTypes("AP", "Automatic Payment", true, false));
+            transactionTypes.Add(new TransactionTypes("IP", "Internet Payment", true, false));
+            transactionTypes.Add(new TransactionTypes("DD", "Direct Deposit", false, true));
+            transactionTypes.Add(new TransactionTypes("D", "Deposit", false, true));
+            transactionTypes.Add(new TransactionTypes("PH", "Phone", true, false));
+            transactionTypes.Add(new TransactionTypes("OD", "Other Debit", true, false));
+            transactionTypes.Add(new TransactionTypes("CK", "Check", true, false));
+            transactionTypes.Add(new TransactionTypes("OC", "Other Credit", false, true));
+
+
+            return transactionTypes.OrderBy(x => x.TransactionTypeCode).ToArray();
+        }
+
+    }
--- MikkiBook2/ViewModels/MainWindowViewModel.cs
+++ MikkiBook2/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,71 @@
-﻿namespace MikkiBook2.ViewModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MikkiBook2.Data;
+
+namespace MikkiBook2.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    /// <summary>The database context</summary>
+    private AccountContext? dbContext;
+    
+    /// <summary>
+    /// New window view model
+    /// </summary>
+    public MainWindowViewModel()
+    {
+        Greeting = "Welcome to Mikki Book 2!";
+        
+        CurrentBalance = $"$0.00";
+        
+        this.dbContext = new AccountContext();
+        this.dbContext.Database.EnsureCreated();
+
+        this.TransactionTypes = new List<TransactionTypes>();
+        this.TransactionTypes.AddRange(Data.TransactionTypes.GetTransactionTypes());
+        this.TransactionTypes = this.TransactionTypes.OrderBy(x => x.TransactionTypeCode).ToList();
+
+        this.TransactionList = new List<AccountTransaction>();
+        this.RebuildTransactions().Wait();
+    }
+
+    /// <summary>
+    /// Greeting message
+    /// </summary>
+    public string Greeting { get; set; }
+    
+    /// <summary>The current identifier</summary>
+    public long CurrentId { get; set; }
+    
+    /// <summary>The current balance</summary>
+    public string CurrentBalance { get; set; }
+    
+    /// <summary>The transaction types</summary>
+    public List<TransactionTypes> TransactionTypes { get; set; }
+    
+    /// <summary>The transaction list</summary>
+    public List<AccountTransaction> TransactionList { get; set; }
+
+    /// <summary>
+    /// Rebuilds the transaction list.
+    /// </summary>
+    private Task RebuildTransactions()
+    {
+        decimal balance = 0.00M;
+
+        using (var tempContext = new AccountContext())
+        {
+            tempContext.Database.EnsureCreated();
+            foreach (var trans in tempContext.AccountTransactions.ToList())
+            {
+                balance = balance + trans.Amount;
+                this.TransactionList.Add((AccountTransaction)trans.Clone());
+            }
+
+            CurrentBalance = $"${balance}";
+        }
+        
+        return Task.CompletedTask;
+    }
 }
