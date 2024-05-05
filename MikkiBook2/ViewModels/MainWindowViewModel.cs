9b37e5030c69500aaa57a88c8a82370c7b2d984a
cmbsolver <cmbsolver@gmail.com>
2024-05-05 00:20:58 -0500

    Wired up the controls

--- MikkiBook2/ViewModels/MainWindowViewModel.cs
+++ MikkiBook2/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using MikkiBook2.Data;
+using ReactiveUI;
 
 namespace MikkiBook2.ViewModels;
 
@@ -18,46 +21,97 @@ public class MainWindowViewModel : ViewModelBase
         Greeting = "Welcome to Mikki Book 2!";
         
         CurrentBalance = $"$0.00";
-        
+
         this.dbContext = new AccountContext();
         this.dbContext.Database.EnsureCreated();
 
-        this.TransactionTypes = new List<TransactionTypes>();
-        this.TransactionTypes.AddRange(Data.TransactionTypes.GetTransactionTypes());
-        this.TransactionTypes = this.TransactionTypes.OrderBy(x => x.TransactionTypeCode).ToList();
+        this.TransactionTypes = new List<TransactionTypes>(
+            Data.TransactionTypes.GetTransactionTypes().OrderBy(x => x.TransactionTypeCode));
 
-        this.TransactionList = new List<AccountTransaction>();
-        this.RebuildTransactions().Wait();
+        this.TransactionList = new ObservableCollection<AccountTransaction>();
     }
 
     /// <summary>
     /// Greeting message
     /// </summary>
     public string Greeting { get; set; }
-    
+
     /// <summary>The current identifier</summary>
-    public long CurrentId { get; set; }
+    private long? currentId;
+    public long? CurrentId
+    {
+        get => currentId;
+        set => this.RaiseAndSetIfChanged(ref currentId, value);
+    }
     
     /// <summary>The current balance</summary>
-    public string CurrentBalance { get; set; }
+    private string? currentBalance;
+    public string? CurrentBalance
+    {
+        get => currentBalance;
+        set => this.RaiseAndSetIfChanged(ref currentBalance, value);
+    }
+
+    private string? currentDescription;
+    public string? CurrentDescription
+    {
+        get => currentDescription;
+        set => this.RaiseAndSetIfChanged(ref currentDescription, value);
+    }
+    
+    private TransactionTypes? currentTransactionType;
+    public TransactionTypes? CurrentTransactionType
+    {
+        get => currentTransactionType;
+        set => this.RaiseAndSetIfChanged(ref currentTransactionType, value);
+    }
+    
+    private DateTimeOffset? currentTransactionDate;
+    public DateTimeOffset? CurrentTransactionDate
+    {
+        get => currentTransactionDate;
+        set => this.RaiseAndSetIfChanged(ref currentTransactionDate, value);
+    }
+    
+    private DateTimeOffset? currentReconciledDate;
+    public DateTimeOffset? CurrentReconciledDate
+    {
+        get => currentReconciledDate;
+        set => this.RaiseAndSetIfChanged(ref currentReconciledDate, value);
+    }
+    
+    private string? currentAmount;
+    public string? CurrentAmount
+    {
+        get => currentAmount;
+        set => this.RaiseAndSetIfChanged(ref currentAmount, value);
+    }
+    
+    private string? currentCheckNumber;
+    public string? CurrentCheckNumber
+    {
+        get => currentCheckNumber;
+        set => this.RaiseAndSetIfChanged(ref currentCheckNumber, value);
+    }
     
     /// <summary>The transaction types</summary>
     public List<TransactionTypes> TransactionTypes { get; set; }
     
     /// <summary>The transaction list</summary>
-    public List<AccountTransaction> TransactionList { get; set; }
+    public ObservableCollection<AccountTransaction> TransactionList { get; set; }
 
     /// <summary>
     /// Rebuilds the transaction list.
     /// </summary>
-    private Task RebuildTransactions()
+    public Task RebuildTransactions()
     {
         decimal balance = 0.00M;
+        this.TransactionList.Clear();
 
         using (var tempContext = new AccountContext())
         {
             tempContext.Database.EnsureCreated();
-            foreach (var trans in tempContext.AccountTransactions.ToList())
+            foreach (var trans in tempContext.AccountTransactions.OrderByDescending(x => x.TransactionDate))
             {
                 balance = balance + trans.Amount;
                 this.TransactionList.Add((AccountTransaction)trans.Clone());
@@ -67,5 +121,23 @@ public class MainWindowViewModel : ViewModelBase
         }
         
         return Task.CompletedTask;
+    }
+    
+    public Task NewRecord()
+    {
+        this.CurrentId = null;
+        this.CurrentDescription = string.Empty;
+        this.CurrentTransactionType = null;
+        this.CurrentTransactionDate = DateTimeOffset.Now;
+        this.CurrentReconciledDate = null;
+        this.CurrentAmount = string.Empty;
+        this.CurrentCheckNumber = string.Empty;
+        
+        return Task.CompletedTask;
+    }
+
+    public Task<int> SaveRecord()
+    {
+        return Task.FromResult(0);
     }
 }
--- MikkiBook2/Views/MainWindow.axaml.cs
+++ MikkiBook2/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using MikkiBook2.ViewModels;
 
 namespace MikkiBook2.Views;
 
@@ -7,5 +9,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var viewModel = (MainWindowViewModel)DataContext;
+        viewModel.RebuildTransactions().Wait();
+    }
+
+    private void New_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var viewModel = (MainWindowViewModel)DataContext;
+        viewModel.NewRecord().Wait();
+    }
+    
+    private void Save_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var viewModel = (MainWindowViewModel)DataContext;
+        viewModel.SaveRecord().Wait();
     }
 }
